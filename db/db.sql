172861fba5d4361e7102666fe08d571972e3bd9a
Phatdat01 <phatdatlht2001@gmail.com>
2024-06-24 08:03:12 +0700

    dat: edit db

--- db/db.sql
+++ db/db.sql
@@ -5,9 +5,9 @@ USE `shop`;
 CREATE TABLE IF NOT EXISTS `shop_item` (
   `id` int AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
-  `locate` varchar(255) NOT NULL
+  `locate` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-)
+);
 
 INSERT INTO `shop_item` (name,locate) VALUES ('A','Ho Chi Minh');
 INSERT INTO `shop_item` (name,locate) VALUES ('B','Ha Noi');
