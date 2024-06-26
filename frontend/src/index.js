0951a32df6ebf005bcc7e9cb249181cd1cab8b9b
Phatdat01 <phatdatlht2001@gmail.com>
2024-06-26 14:50:22 +0700

    dat: temp work with frontend

---
+++ frontend/src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+// import Login from './views/login'; // Ensure the correct path to your Login component
+// import AdminLayout from "layouts/Admin/Admin.js";
+
+// import ThemeContextWrapper from "./components/ThemeWrapper/ThemeWrapper";
+// import BackgroundColorWrapper from "./components/BackgroundColorWrapper/BackgroundColorWrapper";
+// import ForgotPassword from "views/forgetPassword";
+
+ReactDOM.render(
+  document.getElementById("root")
+);
