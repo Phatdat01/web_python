8c02fa3fa0bf70491221e324afdd8bf74ed08288
Phatdat01 <phatdatlht2001@gmail.com>
2024-06-27 15:10:00 +0700

    dat: done show basic frontend react nodejs

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+// import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 // import Login from './views/login'; // Ensure the correct path to your Login component
 // import AdminLayout from "layouts/Admin/Admin.js";
@@ -10,5 +10,6 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 // import ForgotPassword from "views/forgetPassword";
 
 ReactDOM.render(
+  <h1>Toi la Sieu nhan</h1>,
   document.getElementById("root")
 );
